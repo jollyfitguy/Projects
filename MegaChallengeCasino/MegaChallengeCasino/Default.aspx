3215aef6b7be6167154fb4a6a873e0520d36cd5b
jollyfitguy <bensonprice@gmail.com>
2015-11-21 15:52:14 -0700

    First commit

---
+++ MegaChallengeCasino/MegaChallengeCasino/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MegaChallengeCasino.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Image ID="reelImage1" runat="server" Height="150px" />
+        <asp:Image ID="reelImage2" runat="server" Height="150px" />
+        <asp:Image ID="reelImage3" runat="server" Height="150px" />
+        <br />
+        <br />
+        Your Bet:&nbsp;
+        <asp:TextBox ID="betTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="goButton" runat="server" OnClick="goButton_Click" Text="Pull the Lever!" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="moneyLabel" runat="server"></asp:Label>
+        <br />
+        <br />
+        1 Cherry - x2 Your Bet<br />
+        2 Cherries - x3 Your Bet<br />
+        3 Cherries - x4 Your Bet<br />
+        3 7&#39;s - Jackpot - x100 Your Bet<br />
+        However ... if there is even one BAR you win nothing<br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ MegaChallengeCasino/MegaChallengeCasino/Default.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MegaChallengeCasino
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        Random random = new Random();
+        int numberOfImages = 12;
+        double money = 100;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ViewState.Add("money", money);
+                //Generate Reel images
+                pullLever();
+
+                //Set starting money value
+                moneyLabel.Text = string.Format("Player's Money: {0:C}", money);
+                //display starting values
+
+                
+            }
+        }
+
+        protected void goButton_Click(object sender, EventArgs e)
+        {
+            money = (double)ViewState["money"];
+            //take bet
+            double bet = 0;
+            if(tryTakeBet(out bet) && validateBet(bet))
+            {
+                //Generate Reel images
+                pullLever();
+                //subract bet from money
+                updateMoney(-bet);
+            }
+            
+            //Calculate win or lose
+            //calculate winnings
+            //add winnings to money
+            //display result
+            displayMoney();
+        }
+
+        private void pullLever()
+        {
+            reelImage1.ImageUrl = getReelImage(generateRandomReelInt());
+            reelImage2.ImageUrl = getReelImage(generateRandomReelInt());
+            reelImage3.ImageUrl = getReelImage(generateRandomReelInt());
+        }
+
+        private int generateRandomReelInt ()
+        {
+            return random.Next(1, numberOfImages);
+        }
+
+        private string getReelImage(int imageNumber)
+        {
+            return "..\\Images\\" + getImage(imageNumber);
+        }
+        private string getImage(int imageNumber)
+        {
+            if (imageNumber == 1) return "Bar.png";
+            else if (imageNumber == 2) return "Bell.png";
+            else if (imageNumber == 3) return "Cherry.png";
+            else if (imageNumber == 4) return "Clover.png";
+            else if (imageNumber == 5) return "Diamond.png";
+            else if (imageNumber == 6) return "HorseShoe.png";
+            else if (imageNumber == 7) return "Lemon.png";
+            else if (imageNumber == 8) return "Orange.png";
+            else if (imageNumber == 9) return "Plum.png";
+            else if (imageNumber == 10) return "Seven.png";
+            else if (imageNumber == 11) return "Strawberry.png";
+            else if (imageNumber == 12) return "Watermelon.png";
+            return "Not Found";
+        }
+
+        private void updateMoney(double amount)
+        {
+            money = (double)ViewState["money"];
+            money += amount;
+            ViewState["money"] = money;
+        }
+
+        private bool validateBet(double bet)
+        {
+            if (bet <= 0)
+                return false;
+            else
+                return true;
+        }
+        private bool tryTakeBet(out double bet)
+        {
+            if(!double.TryParse(betTextBox.Text, out bet))
+            {
+                resultLabel.Text = "Please enter a valid number";
+                return false;
+            }
+            return true;
+        }
+        private void displayMoney()
+        {
+            moneyLabel.Text = string.Format("Player's Money: {0:C}", (double)ViewState["money"]);
+        }
+        
+    }
+}
