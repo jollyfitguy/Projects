5ff7cb9676e58fd7edfc8967fa5454b65620e65c
jollyfitguy <bensonprice@gmail.com>
2015-11-22 10:46:13 -0700

    Add new challenge to repo

---
+++ ChallengeHeroMonsterClassesPart1/ChallengeHeroMonsterClassesPart1/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeHeroMonsterClassesPart1
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int DamageMaximum { get; set; }
+        public int AttackBonus { get; set; }
+        private Random random = new Random();
+
+        public int Attack()
+        {
+            return random.Next(1, DamageMaximum) + AttackBonus;
+        }
+        public int Defend(int damage)
+        {
+            Health -= damage;
+            return Health;
+        }
+
+
+    }
+}
---
+++ ChallengeHeroMonsterClassesPart1/ChallengeHeroMonsterClassesPart1/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ChallengeHeroMonsterClassesPart1;
+
+namespace ChallengeHeroMonsterClassesPart1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Character hero = new Character();
+
+            hero.Name = "Hercules";
+            hero.Health = 100;
+            hero.DamageMaximum = 10;
+            hero.AttackBonus = 2;
+
+            Character monster = new Character();
+
+            monster.Name = "Hydra";
+            monster.Health = 75;
+            monster.DamageMaximum = 10;
+            monster.AttackBonus = 3;
+            
+            heroAttackLabel.Text = performAttack(hero, monster);            
+            monsterAttackLabel.Text = performAttack(monster, hero);
+
+            displayStats(hero, monster);
+        }
+
+        private void displayStats(Character hero, Character monster)
+        {
+            resultLabel.Text = String.Format("{0}: {1} HP &nbsp;&nbsp;&nbsp; {2}: {3} HP", hero.Name, hero.Health, monster.Name, monster.Health);
+        }
+        private string performAttack(Character attacker, Character defender)
+        {
+            int attack = attacker.Attack();
+            defender.Defend(attack);
+            return String.Format("{0} attacks {1} and deals {2} damage!", attacker.Name, defender.Name, attack);
+            
+        }
+    }
+}
