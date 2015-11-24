110e31e9ec717bf620e94edb0e524dd3e8e6b413
jollyfitguy <bensonprice@gmail.com>
2015-11-24 09:04:27 -0700

    Add Statistics.cs

--- ChallengeSimpleDarts/ChallengeSimpleDarts/Default.aspx.cs
+++ ChallengeSimpleDarts/ChallengeSimpleDarts/Default.aspx.cs
@@ -24,21 +24,37 @@ namespace ChallengeSimpleDarts
 
             //resultLabel.Text = String.Format("Dart landed on {0} with ringmultiplier {1}x", dart.Score, dart.RingMultiplier);
 
-            Game game = new Game();
-            game.Play();
+            int games = 1000000;
 
-            string winningPlayer = "";
+            Statistics player1Stats = new Statistics(games);
+            Statistics player2Stats = new Statistics(games);
 
-            if (game.Player1Score > game.Player2Score)
-                winningPlayer = "Player 1";
-            else
-                winningPlayer = "Player 2";
+            for (int i = 0; i < player1Stats.Length; i++)
+            {
+                Game game = new Game();
+                game.Play();
 
-            resultLabel.Text = String.Format("Player 1: {0}<br/>Player 2: {1}<br/>{2} wins!", 
-                game.Player1Score,
-                game.Player2Score,
-                winningPlayer);
-            
+                if (game.Player1Score > game.Player2Score)
+                {
+                    player1Stats.AddGameResult(1);
+                    player2Stats.AddGameResult(0);
+                }
+                else
+                {
+                    player1Stats.AddGameResult(0);
+                    player2Stats.AddGameResult(1);
+                }
+            }
+
+
+
+
+
+
+            resultLabel.Text = String.Format("Player 1 won : {0}% of the time.<br/>Player 2 won : {1}% of the time.<br/>",
+                player1Stats.GetWinPercentage().ToString(),
+                player2Stats.GetWinPercentage().ToString());
+
         }
     }
 }
---
+++ ChallengeSimpleDarts/ChallengeSimpleDarts/Statistics.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeSimpleDarts
+{
+    public class Statistics
+    {
+        private int[] _winHistory;
+        public int Length { get; private set; }
+        public int Wins { get; private set; }
+        public int Losses { get; private set; }
+        private int _lastIndex;
+
+
+        public Statistics(int games)
+        {
+            _winHistory = new int[games];
+            Length = _winHistory.Length;
+            initializeArray();
+            Wins = 0;
+            Losses = 0;
+            _lastIndex = 0;
+        }
+        private void initializeArray()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                _winHistory[i] = -1;
+            }
+        }
+
+        public bool AddGameResult(int result)
+        {   
+            if (_lastIndex == Length)
+                return false;
+            else
+            {
+                _winHistory[_lastIndex] = result;
+                determineResult(result);
+                return true;
+            }
+        }
+
+        private void determineResult(int result)
+        {
+            if (result == 1)
+                Wins++;
+            else if (result == 0)
+                Losses++;
+        }
+        
+        public double GetWinPercentage()
+        {
+            return 100 * (double)Wins / (double)Length;
+        }
+
+
+    }
+}
